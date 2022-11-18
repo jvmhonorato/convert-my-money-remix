8f3079ef726a006de1edd541aca95a513c3084f3
vituhonorato <vituhonorato.89@gmail.com>
2022-11-18 20:55:23 -0300

    add LoaderFunction and fetch data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,16 @@
+import { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader : LoaderFunction = () => {
+  console.log("loader")
+  return fetch('http://httpbin.org/uuid')
+}
+
 export default function Index() {
+  const {uuid} = useLoaderData()
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <p>UUID: {uuid}</p>
     </div>
   );
 }
